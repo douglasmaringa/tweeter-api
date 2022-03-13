7d079dbf3fa490db2380394981532d92b1e9b707
Douglas Maringa <douglasmaringa4@gmail.com>
2022-03-13 13:47:14 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -85,16 +85,16 @@ console.log(data)
 
 {
   data.map((e)=>(
-    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+    <div key={e.tweet} class="max-w-sm rounded overflow-hidden shadow-lg">
  
-  <div class="px-6 py-4">
-    <div class="font-bold text-xl mb-2">Tweet</div>
-    <p class="text-gray-700 text-base">
+  <div className="px-6 py-4">
+    <div className="font-bold text-xl mb-2">Tweet</div>
+    <p className="text-gray-700 text-base">
      {e.tweet}
     </p>
   </div>
-  <div class="px-6 pt-4 pb-2">
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{e.likes} likes</span>
+  <div className="px-6 pt-4 pb-2">
+    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{e.likes} likes</span>
    
   </div>
 </div>
